a4528e0d010e22cffe6d796f42c894f78b73350f
pelmenkus <kli6900@yandex.ru>
2022-03-09 13:20:00 +0300

    14124143

---
+++ prilozhenie.py
@@ -0,0 +1,30 @@
+from flask import *
+from models import *
+from init import app
+
+@app.route('/')
+def first_page():
+    return render_template("main.html")
+
+@app.route('/catalog')
+def catalog_page():
+    return render_template("catalog.html")
+
+@app.route('/card1')
+def island_spa_page():
+    return render_template("card1.html")
+
+@app.route('/card2')
+def island_spa_page():
+    return render_template("card2.html")
+
+@app.route('/card3')
+def island_spa_page():
+    return render_template("card3.html")
+
+@app.route('/about')
+def island_spa_page():
+    return render_template("about.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
