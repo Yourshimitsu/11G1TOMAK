189433bb77b0cb0f067f64f99fefba9a42542315
pelmenkus <kli6900@yandex.ru>
2022-04-06 13:14:13 +0300

    bugfix

--- prilozhenie.py
+++ prilozhenie.py
@@ -1,5 +1,4 @@
 from flask import *
-from models import *
 from init import app
 
 @app.route('/')
@@ -11,19 +10,19 @@ def catalog_page():
     return render_template("catalog.html")
 
 @app.route('/card1')
-def island_spa_page():
+def island_spa_page1():
     return render_template("card1.html")
 
 @app.route('/card2')
-def island_spa_page():
+def island_spa_page2():
     return render_template("card2.html")
 
 @app.route('/card3')
-def island_spa_page():
+def island_spa_page3():
     return render_template("card3.html")
 
 @app.route('/about')
-def island_spa_page():
+def island_spa_page4():
     return render_template("about.html")
 
 if __name__ == '__main__':
